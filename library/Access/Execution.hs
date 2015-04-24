6dd65c97180953bf01cc47415a1aeee6d2590d65
Geraud Boyer <geraud@gmail.com>
2015-04-23 18:14:31 -0700

    Switch to Mustache for templating

--- library/Access/Execution.hs
+++ library/Access/Execution.hs
@@ -1,46 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Access.Execution where
 
-import           Control.Arrow      (first, (***))
-import           Data.Char          (isAlphaNum, toUpper)
-import qualified Data.Map.Strict    as M
-import           Data.Monoid        ((<>))
-import           Data.Text          (Text)
-import qualified Data.Text          as T
-import qualified Data.Text.IO       as T
-import           System.Environment (getEnvironment)
-import           System.Exit        (exitWith)
-import           System.Process     (CreateProcess (..), StdStream (..), createProcess, shell,
-                                     waitForProcess)
+import qualified Data.Map.Strict       as M
+import           Data.Text             (Text, pack)
+import qualified Data.Text.Lazy        as TL
+import           System.Exit           (exitWith)
+import           System.Process        (CreateProcess (..), StdStream (..), createProcess, shell,
+                                        waitForProcess)
+import           Text.Hastache
+import           Text.Hastache.Context
 
 import           Access.Types
 
 executeCommand :: Text -> [InstanceMetaData] -> IO ()
 executeCommand _ [] = error "No matches found."
 executeCommand cmd (imd:_) = do
-    e <- getEnvironment
-    let processEnv = e <> toEnvironment imd
-        name = imd M.! "name"
-        region = imd M.! "region"
-        public_dns = imd M.! "public_dns"
-        message = "Connecting " <> name <> " in " <> region <> " (" <> public_dns <> ")"
-        cp = (shell $ T.unpack cmd) { std_in = Inherit
-                                    , std_out = Inherit
-                                    , std_err= Inherit
-                                    , env = Just processEnv
-                                    }
-    T.putStrLn message
+    command <- render cmd imd
+    let cp = (shell command) { std_in = Inherit
+                             , std_out = Inherit
+                             , std_err= Inherit
+                             , env = Nothing
+                             }
+
+    putStrLn =<< render "Connecting {{name}} in {{region}} ({{public_dns}})" imd
     (_, _, _, processHandle) <- createProcess cp
     rc <- waitForProcess processHandle
     exitWith rc
 
--- | Convert the insteance meta-data hash to a list.
-toEnvironment :: InstanceMetaData   -- ^ instance meta-data
-              -> [(String, String)] -- ^ list of key/value tuples
-toEnvironment imd = first capitalizeKey . (T.unpack *** T.unpack) <$> M.toList imd
-
--- | The keys are capitalized and are purged of non
--- alpha-numeric character by converting them to underscores.
-capitalizeKey :: String -- ^ input such as aws:cloudformation:stack-name
-              -> String -- ^ ouptut AWS_CLOUDFORMATION_STACK_NAME
-capitalizeKey key = (\c -> if isAlphaNum c then toUpper c else '_') <$> key
+render :: Text -> InstanceMetaData -> IO String
+render cmd imd = TL.unpack <$> hastacheStr defaultConfig cmd (mkStrContext $ handler imd)
+    where handler m name = maybe MuNothing (MuVariable) $ M.lookup (pack name) m
